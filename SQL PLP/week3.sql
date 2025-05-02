7ddead7f20737bae4f6d7403d9bbee9463b37d2f
Kip8980 <cheruiyotkiptooelkan.com>
2025-05-02 12:25:37 +0300

    Add week3.sql assignment file

--- SQL PLP/week3.sql
+++ SQL PLP/week3.sql
@@ -3,3 +3,11 @@ CREATE TABLE student(
     full_name VARCHAR(100),
     age INT,
 )
+
+INSERT INTO student (id, fullName, age) VALUES (1, 'Alice Johnson', 19);
+INSERT INTO student (id, fullName, age) VALUES (2, 'Brian Smith', 18);
+INSERT INTO student (id, fullName, age) VALUES (3, 'Clara Davis', 21);
+
+UPDATE student
+SET age = 20
+WHERE id = 2;
